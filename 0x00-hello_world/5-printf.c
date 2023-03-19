3478fd82bcb07e34779641ba6b4920c6c11f2457
radwa laarif <laarifradwa7@gmail.com>
2023-03-19 13:59:19 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
